0c0293488d8bf5f43b7e992d4409e5d5c59228fa
Torin Sparks <tsparks9038@conestogac.on.ca>
2023-02-10 22:08:15 -0500

    Add project files.

---
+++ RadioCS/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.ComponentModel;
+
+namespace RadioCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (button.Content.ToString() == "Stop")
+            {
+                button.Content = "Start";
+
+                return;
+            }
+
+            button.Content = "Stop";
+
+            new Thread(getData).Start();
+        }
+
+        private void getData()
+        {
+            SongInfo info = new();
+            long end, curr, timeLeft;
+            string[] nameArt;
+
+            while (true)
+            {
+                nameArt = info.GetSongName().Split(" - ");
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    songName.Text = nameArt[1];
+                    artistName.Text = nameArt[0];
+                });
+
+                end = info.GetEndTime();
+                curr = info.GetCurrent();
+
+                timeLeft = end - curr;
+
+                DateTime defaultUnix = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                DateTime timeUnix = defaultUnix.AddSeconds(timeLeft).ToLocalTime();
+
+                DateTimeOffset timeMill = timeUnix;
+
+                int timeInt = (int)timeMill.ToUnixTimeMilliseconds();
+
+                Thread.Sleep(timeInt + 2000);
+            }
+        }
+    }
+}
---
+++ RadioCS/SongInfo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RadioCS
+{
+    internal class SongInfo
+    {
+        public string GetSongName()
+        {
+            HttpClient client = new();
+            string result;
+
+            result = client.GetStringAsync("https://r-a-d.io/api").Result;
+
+            JsonDocument json = JsonDocument.Parse(result);
+            JsonElement root = json.RootElement;
+            JsonElement main = root.GetProperty("main");
+            JsonElement np = main.GetProperty("np");
+
+            return np.GetString();
+        }
+
+        public long GetStartTime()
+        {
+            HttpClient client = new();
+            string result;
+
+            result = client.GetStringAsync("https://r-a-d.io/api").Result;
+
+            JsonDocument json = JsonDocument.Parse(result);
+            JsonElement root = json.RootElement;
+            JsonElement main = root.GetProperty("main");
+            JsonElement start_time = main.GetProperty("start_time");
+
+            return start_time.GetInt64();
+        }
+
+        public long GetEndTime()
+        {
+            HttpClient client = new();
+            string result;
+
+            result = client.GetStringAsync("https://r-a-d.io/api").Result;
+
+            JsonDocument json = JsonDocument.Parse(result);
+            JsonElement root = json.RootElement;
+            JsonElement main = root.GetProperty("main");
+            JsonElement end_time = main.GetProperty("end_time");
+
+            return end_time.GetInt64();
+        }
+
+        public long GetCurrent()
+        {
+            HttpClient client = new();
+            string result;
+
+            result = client.GetStringAsync("https://r-a-d.io/api").Result;
+
+            JsonDocument json = JsonDocument.Parse(result);
+            JsonElement root = json.RootElement;
+            JsonElement main = root.GetProperty("main");
+            JsonElement current = main.GetProperty("current");
+
+            return current.GetInt64();
+        }
+    }
+}
